0c4fb428989d5fb98a4934dfb58f3a9d29cc2cd3
Asi-Web <agblezea@students.trinitydc.edu>
2026-02-21 22:31:09 -0500

    Add Milestone 1 JAWG Contacts description and contact imput

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # JAWG-Contacts
  Research contact tracker
+
+## Asiwome Agbleze
+## CMSC 1111
+## Spring 2026
+## Capston Project 1
+## Project Idea and Purpose
+I want to create a simple contact manager that would keep track of people and organizations I work with during my internship. The name of this project is JAWG Contacts and it would have a structured list of contacts that the Python program can read, be able to update and display information. Important details like the person’s name, organization, role, email address, and a short description of how we are connected to each other. The data would be saved in a simple format like a list of dictionaries, in a text format, CSV, or JSON file. 
+
+This project would allow me to add a new partners, see all partners, and look up a specific person, organization, editing an existing contact, deleting a contact that is no longer active, or filtering by type of partner (like, “organizational name”, “provider”, interviewee” etc).
+
+## Planned Features
+Add a new partner with contact details
+See a list of all partners
+Look up a specific partner by name or organization
+Edit an existing contract
+Delect a contact that is no longer existing
+Filter partners by type (organization, provider, interviewee)
---
+++ jawg_contacts.py
@@ -0,0 +1,48 @@
+# JAWG Contacts -Milestone 1
+# Simple text-based contact tracker for internship partners
+
+# 1. Greet user and explain what the program is about
+print("Welcome to JAWG Contacts!")
+print("This simple program helps you track people and organizations from JAWG.\n")
+
+
+# 2. Inquire what the user wants 
+print("What would you like to do?")
+print("1. Add a new contact")
+print("2. View an example contact")
+
+action_choice = input("Enter 1 or 2: ")
+
+# 3. Use if/ elif/ else to choose what happens
+if action_choice == "1":
+    #User wants to add a new contact
+    print("\nGreat! Let's add a new contact.\n")
+
+    contact_name = input("Contact's name: ")
+    organization = input("Organization: ")
+    role = input("Role (for example: mentor, provider, interviewee): ")
+    email = input("Email address: ")
+    connection_not = input("How are you connected to this person or organization? ")
+
+    # 4. Show a summary using the variables
+    print("\nYou added this contact to JAWG Contacts:")
+    print(f"Name: {contact_name}")
+    print(f"Organization: {organization}")
+    print(f"Role: {role}")
+    print(f"Email: {email}")
+    print(f"Connection: {"connection_note"}")
+
+elif action_choice == "2:":
+    # User would chose to view an example instead of adding a contact
+    print("\nExample contact in JAWG Contacts:")
+    print("Name: Dr. Wairimu Mwangi")
+    print("Organization: JAWG Community Healt Clinic")
+    print("Role: Mentor")
+    print("Email: mwangiwairimutri@gmail.com")
+    print("Connection: Supervises my internship project.")
+
+else:
+    # User typed something other than 1 or 2
+    print("\nThat was not a valid option.")
+    print("Please run JAWG Contacts again and choose 1 or 2.")
+
